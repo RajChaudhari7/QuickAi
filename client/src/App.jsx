7ca3670e5e8c31725432c502183b4d8c55056def
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-03-13 22:07:16 +0530

    updated blogtitle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home'
 import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
 import WriteArticle from './pages/WriteArticle'
-import BlogTitles from './pages/BlogTitles'
 import GenerateImages from './pages/GenerateImages'
 import RemoveBackground from './pages/RemoveBackground'
 import ReviewResume from './pages/ReviewResume'
@@ -21,7 +20,6 @@ const App = () => {
         <Route path='/ai' element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path='write-article' element={<WriteArticle />} />
-          <Route path='blog-titles' element={<BlogTitles />} />
           <Route path='generate-images' element={<GenerateImages />} />
           <Route path='remove-background' element={<RemoveBackground />} />
           <Route path='review-resume' element={<ReviewResume />} />
